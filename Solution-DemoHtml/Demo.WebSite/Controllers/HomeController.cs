3596b9f5a603b1df1e41bc47bedc731a3756c4a3
Ricardo Roberto Goffi <rrgoffi@gmail.com>
2020-05-31 16:41:31 -0300

    Fin ejercicio 1

---
+++ Solution-DemoHtml/Demo.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+    }
+}
